58cb478007725deefaa3c6020494e58cbe07cd17
Aryaman Parekh <aryamanparekh12@gmail.com>
2025-10-18 02:07:46 -0700

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    https: {
+      key: fs.readFileSync(path.join(__dirname, "certs", "key.pem")),
+      cert: fs.readFileSync(path.join(__dirname, "certs", "cert.pem")),
+    },
+  },
 });
