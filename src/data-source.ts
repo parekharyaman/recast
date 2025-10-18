55665e94907b29e836b0ef3ac2d9e1fd690fb4de
Aryaman Parekh <aryamanparekh12@gmail.com>
2025-10-17 23:22:58 -0700

    finish up

---
+++ src/data-source.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/*.ts"],
+  migrations: ["src/migration/*.ts"],
+  subscribers: [],
+});
