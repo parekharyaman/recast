06ec81ac516b36c487d787db18966c97083f6abc
Aryaman Parekh <aryamanparekh12@gmail.com>
2025-10-18 00:04:32 -0700

    connect fe with backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:5678")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <>
       <div>Recast</div>
+      <div>
+        {users.map((user: User) => (
+          <div key={user.id}>{user.firstName}</div>
+        ))}
+      </div>
     </>
   );
 }
