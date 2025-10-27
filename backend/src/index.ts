d97f20c681d6f27ae72f1fc8f4a598d8e08856dd
Aryaman Parekh <aryamanparekh12@gmail.com>
2025-10-26 20:36:25 -0700

    bunch of changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -49,4 +49,4 @@ AppDataSource.initialize()
       console.log(`Server is running on port ${PORT}`);
     });
   })
-  .catch((error) => console.log(error));
+  .catch((error: any) => console.log(error));
